0966f628ad205b0fa460037005c930ba8c48bc76
Samira-Hoummane <samirahoummane@gmail.com>
2021-11-04 15:01:57 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ const answers=document.querySelector(".questions__answers");
 const question=document.querySelector(".questions__question");
 const previousBtn=document.querySelector('.buttons__btns1');
 const nextBtn=document.querySelector('.buttons__btns2');
-const infoPage=document.getElementById('shown');
-const testPage= document.getElementById('hidden');
+const infoPage=document.getElementById('shown').style.display='block';
+const testPage= document.getElementById('hidden').style.display='none';
 const testStart=document.getElementById('btnquiz');
 const Tab=["", "Pensez-vous avoir eu de la fièvre ces dernièrs jours (frissons, sueurs) ?", "Si oui : quelle est votre température ?", "Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ?", "Avez-vous des douleurs musculaires ou des courbatures inhabituelles ces derniers jours ?", "Avez-vous un mal de gorge apparu ces derniers jours ?", "Avez-vous de la diarrhée ces dernières 24 heures (au moins 3 selles molles) ?", "Avez-vous une fatigue inhabituelle ces derniers jours ?", "Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?", "Avez-vous vu apparaître une gêne respiratoire ou une augmentation de votre gêne respiratoire habituelle ?", "Comment vous sentez-vous ?", "Avez-vous d’autre symptôme ?", "Quel est votre âge ?", "Quel est votre poids ? Quelle est votre taille ?", "Avez-vous de l’hypertension artérielle ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez vous un traitement à visée cardiologique ?", "Êtes-vous diabétique ?", "Avez-vous ou avez-vous eu un cancer ?", "Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?", "Avez-vous une insuffisance rénale chronique dialysée ?", "Avez-vous une maladie chronique du foie ?", "Êtes-vous enceinte ?", "Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?", "Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive)."];
 
@@ -17,7 +17,7 @@ introduction.innerHTML = `<div class="information">
 <hr>
 <p class="information__info">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere quidem a, accusamus minima, cupiditate quasi perspiciatis repellat consequatur illo, qui praesentium optio. Veritatis cum, id at incidunt quo magni quam?</p>
 </div>
-</div>`
+</div>
 
 ;
 })*/
@@ -30,12 +30,16 @@ introduction.innerHTML = `<div class="information">
 
 
 
-    testPage.classList.add('hidden'),
+    /*testPage.classList.add('hidden'),
     infoPage.classList.add('shown');
     testStart.addEventListener('click', ()=>{
         infoPage.classList.add('hidden');
         testPage.classList.add('shown');
-    })
+    })*/
+
+   
+    
+    
     
 
 
