8e04352dc9bd6ba69ae6b0409df3b9b0f2a8b103
Samira-Hoummane <samirahoummane@gmail.com>
2021-11-05 14:00:00 +0100

    questionnaire modif

--- js/script.js
+++ js/script.js
@@ -6,32 +6,67 @@ const nextBtn=document.querySelector('.buttons__btns2');
 const infoPage=document.getElementById('shown');
 const testPage= document.getElementById('hidden');
 const testStart=document.getElementById('btnquiz');
-const Tab=["","Pensez-vous avoir eu de la fièvre ces dernièrs jours (frissons, sueurs) ?", "Si oui : quelle est votre température ?", "Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ?", "Avez-vous des douleurs musculaires ou des courbatures inhabituelles ces derniers jours ?", "Avez-vous un mal de gorge apparu ces derniers jours ?", "Avez-vous de la diarrhée ces dernières 24 heures (au moins 3 selles molles) ?", "Avez-vous une fatigue inhabituelle ces derniers jours ?", "Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?", "Avez-vous vu apparaître une gêne respiratoire ou une augmentation de votre gêne respiratoire habituelle ?", "Comment vous sentez-vous ?", "Avez-vous d’autre symptôme ?", "Quel est votre âge ?", "Quel est votre poids ? Quelle est votre taille ?", "Avez-vous de l’hypertension artérielle ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez vous un traitement à visée cardiologique ?", "Êtes-vous diabétique ?", "Avez-vous ou avez-vous eu un cancer ?", "Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?", "Avez-vous une insuffisance rénale chronique dialysée ?", "Avez-vous une maladie chronique du foie ?", "Êtes-vous enceinte ?", "Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?", "Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive)."];
+
+// const resultat=document.getElementsById('result');
+//const resultBtn=document.getElementsById('btnresult');
+const questionList=["","Pensez-vous avoir eu de la fièvre ces dernièrs jours (frissons, sueurs) ?", "Si oui : quelle est votre température ?", "Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ?", "Avez-vous des douleurs musculaires ou des courbatures inhabituelles ces derniers jours ?", "Avez-vous un mal de gorge apparu ces derniers jours ?", "Avez-vous de la diarrhée ces dernières 24 heures (au moins 3 selles molles) ?", "Avez-vous une fatigue inhabituelle ces derniers jours ?", "Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?", "Avez-vous vu apparaître une gêne respiratoire ou une augmentation de votre gêne respiratoire habituelle ?", "Comment vous sentez-vous ?", "Avez-vous d’autre symptôme ?", "Quel est votre âge ?", "Quel est votre poids ? Quelle est votre taille ?", "Avez-vous de l’hypertension artérielle ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez vous un traitement à visée cardiologique ?", "Êtes-vous diabétique ?", "Avez-vous ou avez-vous eu un cancer ?", "Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?", "Avez-vous une insuffisance rénale chronique dialysée ?", "Avez-vous une maladie chronique du foie ?", "Êtes-vous enceinte ?", "Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?", "Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive)."];
 
 
 window.addEventListener("DOMContentLoaded", ()=>{
 infoPage.style.display='block';
 testPage.style.display='none';
 
+
 testStart.addEventListener('click', ()=>{
         
         infoPage.style.display = 'none';
         testPage.style.display = 'block';
+
+        
         
     })
     
 })
 
+
+/*const counter=document.getElementsById("questioncounter");
+const progressq=document.getElementsById("questionquiz");
+
+const MaxQuestions= 22;
+let availableQuestions=[];
+let counter=0;
+
+
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || counter >= MAX_QUESTIONS) {
+     
+      return window.location.assign("/end.html");
+    }
+    counter++;
+    counterText.innerText=counter+ "/" + MaxQuestions;
+    /*progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
     
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+  
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
+    question.innerText = currentQuestion.question;
+    }*/
+
+
+
+
+
+
 
 let i=1;
 nextBtn.addEventListener("click", function(){
     i++;
-    if(i>Tab.length-1)
+    if(i>questionList.length-1)
     {
-        i=Tab.length-1;
+        i=questionList.length-1;
     }
-    if(i!=1 &&i!=2 && i!=10 && i!=11 && i!=12 && i!=13 && i!=14 && i!=20 && i!=21 && i!=22)
+    if(i!=1 &&i!=2 && i!=10 && i!=11 && i!=12 && i!=13 && i!=14 && i!=20 && i!=21 && i!=22 && i!=23)
     {
         answers.innerHTML=`
         <div class="questions__answer">
@@ -43,6 +78,7 @@ nextBtn.addEventListener("click", function(){
                     <label for="button"> Non</label>
                 </div>
         `;
+        nextBtn.innerText="Question suivante";
     }
     else if(i==1)
     {
@@ -58,6 +94,7 @@ nextBtn.addEventListener("click", function(){
         <input type="radio" class="questions__butn" value="answers">
         <label for="button"> Ne sait pas</label>
     </div>`;
+    nextBtn.innerText="Question suivante";
     }
     else if(i==2)
     {
@@ -74,6 +111,7 @@ nextBtn.addEventListener("click", function(){
         <label for="degree">Non renseignée & OUI à question précédente</label>
     </div>
 </div>`;
+nextBtn.innerText="Question suivante";
     }
     else if(i==10)
     {
@@ -93,11 +131,13 @@ nextBtn.addEventListener("click", function(){
         <input type="radio" id="tresmal" class="questions__butn">
         <label for="tresmal">Très mal</label>
     </div>`;
+    nextBtn.innerText="Question suivante";
     }
     else if(i==11)
     {
         answers.innerHTML=`<div class="questions__answer">
         <input type="text" id="answer"> </div>`;
+        nextBtn.innerText="Question suivante";
     }
     else if(i==12)
     {
@@ -113,12 +153,14 @@ nextBtn.addEventListener("click", function(){
         <input type="radio" id="age3" class="questions__butn">
         <label for="minors"> STOP si < 15 ans : Prenez contact avec votre médecin généraliste au moindre doute. Cette application n’est pour l’instant pas adaptée aux personnes de moins de 15 ans. En cas d’urgence, appeler le 15.</label>
     </div>`;
+    nextBtn.innerText="Question suivante";
     }
     else if(i==13)
     {
         answers.innerHTML=`<div class="questions__answer">
         <input type="text" id="answer1">
         <input type="text" id="answer2"> </div>`;
+        nextBtn.innerText="Question suivante";
     }
     else if (i==14)
     {
@@ -134,6 +176,7 @@ nextBtn.addEventListener("click", function(){
         <input type="radio" class="questions__butn" value="answers">
         <label for="button"> Ne sait pas</label>
     </div>`;
+    nextBtn.innerText="Question suivante";
     }
     else if(i==20)
     {
@@ -149,6 +192,7 @@ nextBtn.addEventListener("click", function(){
         <input type="radio" class="questions__butn" value="answers">
         <label for="button"> Non applicable</label>
     </div>`;
+    nextBtn.innerText="Question suivante";
     }
     else if(i==21)
     {
@@ -163,7 +207,8 @@ nextBtn.addEventListener("click", function(){
     <div class="questions__answer">
         <input type="radio" class="questions__butn" value="answers">
         <label for="button"> Ne sait pas</label>
-    </div>`
+    </div>`;
+    nextBtn.innerText="Question suivante";
     }
     else if(i==22)
     {
@@ -178,8 +223,20 @@ nextBtn.addEventListener("click", function(){
     <div class="questions__answer">
         <input type="radio" class="questions__butn" value="answers">
         <label for="button"> Ne sait pas</label>
-    </div>`
-    }
+    </div>`,
+    nextBtn.innerText="Envoyer";
+    nextBtn.value="submit";
+    testStart.addEventListener('click', ()=>{
+    document.getElementsById("nextbtn").setAttribute("id","subbtn");
+    
+    })
+    const submitbtn=document.getElementsById("subbtn");
+    submitbtn.addEventListener('click', () =>{
+    testPage.style.display='none';
+})
+    
+}
+    
     else if (i==0)
     {
 
@@ -187,7 +244,7 @@ nextBtn.addEventListener("click", function(){
 
     answers.classList.add(`.questions__answers'${i}`);
     answers.classList.remove(`.questions__answers'${i-1}`);
-    question.innerText=Tab[i];
+    question.innerText=questionList[i];
     
 })
 
@@ -209,6 +266,7 @@ previousBtn.addEventListener("click", function(){
                     <label for="button"> Non</label>
                 </div>
         `;
+        nextBtn.innerText="Question suivante";
     }
     else if(i==1)
     {
@@ -224,6 +282,7 @@ previousBtn.addEventListener("click", function(){
         <input type="radio" class="questions__butn" value="answers">
         <label for="button"> Ne sait pas</label>
     </div>`;
+    nextBtn.innerText="Question suivante";
     }
     else if(i==2)
     {
@@ -240,6 +299,7 @@ previousBtn.addEventListener("click", function(){
         <label for="degree">Non renseignée & OUI à question précédente</label>
     </div>
 </div>`;
+nextBtn.innerText="Question suivante";
     }
     else if(i==10)
     {
@@ -259,11 +319,13 @@ previousBtn.addEventListener("click", function(){
         <input type="radio" id="tresmal" class="questions__butn">
         <label for="tresmal">Très mal</label>
     </div>`;
+    nextBtn.innerText="Question suivante";
     }
     else if(i==11)
     {
         answers.innerHTML=`<div class="questions__answer">
         <input type="text" id="answer"> </div>`;
+        nextBtn.innerText="Question suivante";
     }
     else if(i==12)
     {
@@ -279,12 +341,14 @@ previousBtn.addEventListener("click", function(){
         <input type="radio" id="age3" class="questions__butn">
         <label for="minors"> STOP si < 15 ans : Prenez contact avec votre médecin généraliste au moindre doute. Cette application n’est pour l’instant pas adaptée aux personnes de moins de 15 ans. En cas d’urgence, appeler le 15.</label>
     </div>`;
+    nextBtn.innerText="Question suivante";
     }
     else if(i==13)
     {
         answers.innerHTML=`<div class="questions__answer">
         <input type="text" id="answer1">
         <input type="text" id="answer2"> </div>`;
+        nextBtn.innerText="Question suivante";
     }
     else if (i==14)
     {
@@ -300,6 +364,7 @@ previousBtn.addEventListener("click", function(){
         <input type="radio" class="questions__butn" value="answers">
         <label for="button"> Ne sait pas</label>
     </div>`;
+    nextBtn.innerText="Question suivante";
     }
     else if(i==20)
     {
@@ -315,6 +380,7 @@ previousBtn.addEventListener("click", function(){
         <input type="radio" class="questions__butn" value="answers">
         <label for="button"> Non applicable</label>
     </div>`;
+    nextBtn.innerText="Question suivante";
     }
     else if(i==21)
     {
@@ -329,7 +395,8 @@ previousBtn.addEventListener("click", function(){
     <div class="questions__answer">
         <input type="radio" class="questions__butn" value="answers">
         <label for="button"> Ne sait pas</label>
-    </div>`
+    </div>`;
+    nextBtn.innerText="Question suivante";
     }
     else if(i==22)
     {
@@ -345,11 +412,59 @@ previousBtn.addEventListener("click", function(){
         <input type="radio" class="questions__butn" value="answers">
         <label for="button"> Ne sait pas</label>
     </div>`
+    nextBtn.innerText="Envoyer";
+    nextBtn.value="submit";
+    
     }
     answers.classList.add(`.questions__answers'${i}`);
     answers.classList.remove(`.questions__answers'${i+1}`);
-    question.innerText=Tab[i];
+    question.innerText=questionList[i];
+
+    testStart.addEventListener('click', ()=>{
+        document.getElementsById('nextbtn').id='subbtn';
+        
+        })
+        const submitbtn=document.getElementsById('subbtn');
+        submitbtn.addEventListener('click', () =>{
+            if(i=22){
+                testPage.style.display='none';
+            }
+        })
+
+
 })
 
+
+
+
+
+
+/*const submitbtn=document.getElementsById('subbtn');
+submitbtn.addEventListener('click', () =>{
+    testPage.style.display='none';
+})*/
+
+/*const counter=document.getElementsById("questioncounter");
+const progressq=document.getElementsById("questionquiz");
+
+const MaxQuestions= 22;
+let availableQuestions=[];
+let counter=0;
+
+
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || counter >= MAX_QUESTIONS) {
+     
+      return window.location.assign("/end.html");
+    }
+    counter++;
+    counterText.innerText=counter+ "/" + MaxQuestions;
+    /*progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+    
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+  
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
+    question.innerText = currentQuestion.question;*/
 
 
