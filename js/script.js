69b0dce7814fb0e72dab9b1f9477d7cd48bc3e37
Samira-Hoummane <samirahoummane@gmail.com>
2021-11-06 08:47:04 +0100

    progressbar modifications

--- js/script.js
+++ js/script.js
@@ -8,14 +8,19 @@ const testPage= document.getElementById('hidden');
 const testStart=document.getElementById('btnquiz');
 const textProgressQ=document.getElementById('questionquiz');
 const progressBarFiller=document.getElementById('fill');
+const progressbar=document.getElementById('percentage');
+//const bar=document.getElementById('bar');
 
-const questionList=["","Pensez-vous avoir eu de la fièvre ces dernièrs jours (frissons, sueurs) ?", "Si oui : quelle est votre température ?", "Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ?", "Avez-vous des douleurs musculaires ou des courbatures inhabituelles ces derniers jours ?", "Avez-vous un mal de gorge apparu ces derniers jours ?", "Avez-vous de la diarrhée ces dernières 24 heures (au moins 3 selles molles) ?", "Avez-vous une fatigue inhabituelle ces derniers jours ?", "Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?", "Avez-vous vu apparaître une gêne respiratoire ou une augmentation de votre gêne respiratoire habituelle ?", "Comment vous sentez-vous ?", "Avez-vous d’autre symptôme ?", "Quel est votre âge ?", "Quel est votre poids ? Quelle est votre taille ?", "Avez-vous de l’hypertension artérielle ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez vous un traitement à visée cardiologique ?", "Êtes-vous diabétique ?", "Avez-vous ou avez-vous eu un cancer ?", "Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?", "Avez-vous une insuffisance rénale chronique dialysée ?", "Avez-vous une maladie chronique du foie ?", "Êtes-vous enceinte ?", "Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?", "Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive)."];
+
+const questionList=["","Pensez-vous avoir eu de la fièvre ces dernièrs jours (frissons, sueurs) ?", "Si oui : quelle est votre température ?", "Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ?", "Avez-vous des douleurs musculaires ou des courbatures inhabituelles ces derniers jours ?", "Avez-vous un mal de gorge apparu ces derniers jours ?", "Avez-vous de la diarrhée ces dernières 24 heures (au moins 3 selles molles) ?", "Avez-vous une fatigue inhabituelle ces derniers jours ?", "Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?", "Avez-vous vu apparaître une gêne respiratoire ou une augmentation de votre gêne respiratoire habituelle ?", "Comment vous sentez-vous ?", "Avez-vous d’autres symptômes ?", "Quel est votre âge ?", "Quel est votre poids ? Quelle est votre taille ?", "Avez-vous de l’hypertension artérielle ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez vous un traitement à visée cardiologique ?", "Êtes-vous diabétique ?", "Avez-vous ou avez-vous eu un cancer ?", "Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?", "Avez-vous une insuffisance rénale chronique dialysée ?", "Avez-vous une maladie chronique du foie ?", "Êtes-vous enceinte ?", "Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?", "Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive)."];
 
 
 
 window.addEventListener("DOMContentLoaded", ()=>{
 infoPage.style.display='block';
 testPage.style.display='none';
+progressbar.style.display='none';
+
 
 
 //let i=0;
@@ -25,6 +30,8 @@ testStart.addEventListener('click', ()=>{
         infoPage.style.display = 'none';
         
         testPage.style.display = 'block';
+        progressbar.style.display='block';
+    
         
         
     })
@@ -128,7 +135,7 @@ nextBtn.innerText="Question suivante";
     else if(i==11)
     {
         answers.innerHTML=`<div class="questions__answer">
-        <input type="text" id="answer"> </div>`;
+        <input type="text" id="answer" placeholder="Autres syptômes"> </div>`;
         nextBtn.innerText="Question suivante";
     }
     else if(i==12)
@@ -150,8 +157,8 @@ nextBtn.innerText="Question suivante";
     else if(i==13)
     {
         answers.innerHTML=`<div class="questions__answer">
-        <input type="text" id="answer1">
-        <input type="text" id="answer2"> </div>`;
+        <input type="text" id="answer" placeholder="Poids en kg">
+        <input type="text" id="answer" placeholder="Taille en cm"> </div>`;
         nextBtn.innerText="Question suivante";
     }
     else if (i==14)
@@ -235,6 +242,7 @@ nextBtn.innerText="Question suivante";
      const questionProgress = document.getElementById("questionquiz");
         
         questionProgress.style.display="none";
+        progressbar.style.display='none';
 
     })
 
@@ -334,7 +342,7 @@ nextBtn.innerText="Question suivante";
     else if(i==11)
     {
         answers.innerHTML=`<div class="questions__answer">
-        <input type="text" id="answer"> </div>`;
+        <input type="text" id="answer" placeholder="Autres syptômes"> </div>`;
         nextBtn.innerText="Question suivante";
     }
     else if(i==12)
@@ -356,8 +364,8 @@ nextBtn.innerText="Question suivante";
     else if(i==13)
     {
         answers.innerHTML=`<div class="questions__answer">
-        <input type="text" id="answer1">
-        <input type="text" id="answer2"> </div>`;
+        <input type="text" id="answer1" placeholder="Poids en kg">
+        <input type="text" id="answer2" placeholder="Taille en cm"> </div>`;
         nextBtn.innerText="Question suivante";
     }
     else if (i==14)
@@ -452,7 +460,12 @@ nextBtn.innerText="Question suivante";
 })
 
 
+function changeElement (answer){
+    const input=document.getElementById(answer);
+    input.style.backgroundColor="$textColor9";
+    input.style.border="1px solid $textColor2";
 
+}
 
     
 
