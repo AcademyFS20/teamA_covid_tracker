3a432bfa529aa4ab8d2b86011f98c1e96a8ceb4c
Samira-Hoummane <samirahoummane@gmail.com>
2021-11-06 02:11:12 +0100

    progressbar

--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@ const nextBtn=document.querySelector('.buttons__btns2');
 const infoPage=document.getElementById('shown');
 const testPage= document.getElementById('hidden');
 const testStart=document.getElementById('btnquiz');
-
+const textProgressQ=document.getElementById('questionquiz');
+const progressBarFiller=document.getElementById('fill');
 
 const questionList=["","Pensez-vous avoir eu de la fièvre ces dernièrs jours (frissons, sueurs) ?", "Si oui : quelle est votre température ?", "Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ?", "Avez-vous des douleurs musculaires ou des courbatures inhabituelles ces derniers jours ?", "Avez-vous un mal de gorge apparu ces derniers jours ?", "Avez-vous de la diarrhée ces dernières 24 heures (au moins 3 selles molles) ?", "Avez-vous une fatigue inhabituelle ces derniers jours ?", "Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?", "Avez-vous vu apparaître une gêne respiratoire ou une augmentation de votre gêne respiratoire habituelle ?", "Comment vous sentez-vous ?", "Avez-vous d’autre symptôme ?", "Quel est votre âge ?", "Quel est votre poids ? Quelle est votre taille ?", "Avez-vous de l’hypertension artérielle ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez vous un traitement à visée cardiologique ?", "Êtes-vous diabétique ?", "Avez-vous ou avez-vous eu un cancer ?", "Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?", "Avez-vous une insuffisance rénale chronique dialysée ?", "Avez-vous une maladie chronique du foie ?", "Êtes-vous enceinte ?", "Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?", "Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive)."];
+
 
 
 window.addEventListener("DOMContentLoaded", ()=>{
@@ -16,41 +18,22 @@ infoPage.style.display='block';
 testPage.style.display='none';
 
 
-
+//let i=0;
+//let maxQuestions=22;
 testStart.addEventListener('click', ()=>{
         
         infoPage.style.display = 'none';
         
         testPage.style.display = 'block';
         
+        
     })
+    
+    //questionCounterText.innerText=i+"/"+maxQuestions;
     
 })
 
 
-/*const counter=document.getElementById("questioncounter");
-const progressq=document.getElementById("questionquiz");
-
-const MaxQuestions= 22;
-let availableQuestions=[];
-let counter=0;
-
-
-getNewQuestion = () => {
-    if (availableQuestions.length === 0 || counter >= MAX_QUESTIONS) {
-     
-      return window.location.assign("/end.html");
-    }
-    counter++;
-    counterText.innerText=counter+ "/" + MaxQuestions;}
-    /*progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-    
-    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
-  
-    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
-    currentQuestion = availableQuesions[questionIndex];
-    question.innerText = currentQuestion.question;
-    }*/
 
 
 
@@ -58,9 +41,19 @@ getNewQuestion = () => {
 
 
 
+
+
+//let questionCounter=0;
+let maxQuestions=22;
 let i=1;
 nextBtn.addEventListener("click", function(){
+    
+    textProgressQ.innerText=`Question ${i}/${maxQuestions}`;
+    progressBarFiller.style.width= `${(i/maxQuestions)*100}%`;
+
+
     i++;
+    
     if(i>questionList.length-1)
     {
         i=questionList.length-1;
@@ -244,6 +237,8 @@ nextBtn.innerText="Question suivante";
         questionProgress.style.display="none";
 
     })
+
+   
  
 }
     
@@ -261,6 +256,10 @@ nextBtn.innerText="Question suivante";
 
 previousBtn.addEventListener("click", function(){
     i--;
+    textProgressQ.innerText=`Question ${i}/${maxQuestions}`;
+    progressBarFiller.style.width= `${(i/maxQuestions)*100}%`;
+
+
     if(i<1)
     {
         i=1;
@@ -455,37 +454,6 @@ nextBtn.innerText="Question suivante";
 
 
 
-
-
-// const submitbtn=document.getElementById('subbtn');
-// submitbtn.addEventListener('click', () =>{
-//     testPage.style.display='none';
-// })
-
-// const stepCounter=document.getElementById("questioncounter");
-// const progressq=document.getElementById("questionquiz");
-
-// const MaxQuestions= 22;
-// let availableQuestions=[];
-// let steps=0;
-
-
-// getNewQuestion = () => {
-//     if (availableQuestions.length === 0 || stepCounter >= MaxQuestions) {
-     
-//       return window.location.assign("/end.html");
-//     }
-//     counter++;
-//     counterText.innerText=steps+ "/" + MaxQuestions;
-
-// }
-    /*progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-    
-    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
-  
-    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
-    currentQuestion = availableQuesions[questionIndex];
-    question.innerText = currentQuestion.question;*/
     
 
 
