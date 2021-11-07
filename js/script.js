905e208c40e58ab9a6a4a62895648fbd9b2a05ea
Samira-Hoummane <samirahoummane@gmail.com>
2021-11-08 00:46:44 +0100

    stepper done

--- js/script.js
+++ js/script.js
@@ -9,10 +9,13 @@ const testStart=document.getElementById('btnquiz');
 const textProgressQ=document.getElementById('questionquiz');
 const progressBarFiller=document.getElementById('fill');
 const progressbar=document.getElementById('percentage');
-//const bar=document.getElementById('bar');
+const bullets=document.querySelectorAll('.progressbar__bullet');
+const bulletOne=document.getElementById('one');
+const bulletTwo=document.getElementById('two');
+const bulletThree=document.getElementById('three');
 
 
-const questionList=["","Pensez-vous avoir eu de la fièvre ces dernièrs jours (frissons, sueurs) ?", "Si oui : quelle est votre température ?", "Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ?", "Avez-vous des douleurs musculaires ou des courbatures inhabituelles ces derniers jours ?", "Avez-vous un mal de gorge apparu ces derniers jours ?", "Avez-vous de la diarrhée ces dernières 24 heures (au moins 3 selles molles) ?", "Avez-vous une fatigue inhabituelle ces derniers jours ?", "Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?", "Avez-vous vu apparaître une gêne respiratoire ou une augmentation de votre gêne respiratoire habituelle ?", "Comment vous sentez-vous ?", "Avez-vous d’autres symptômes ?", "Quel est votre âge ?", "Quel est votre poids ? Quelle est votre taille ?", "Avez-vous de l’hypertension artérielle ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez vous un traitement à visée cardiologique ?", "Êtes-vous diabétique ?", "Avez-vous ou avez-vous eu un cancer ?", "Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?", "Avez-vous une insuffisance rénale chronique dialysée ?", "Avez-vous une maladie chronique du foie ?", "Êtes-vous enceinte ?", "Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?", "Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive)."];
+const questionList=["","Pensez-vous avoir eu de la fièvre ces dernièrs jours (frissons, sueurs) ?", "Si oui : quelle est votre température ?", "Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ?", "Avez-vous des douleurs musculaires ou des courbatures inhabituelles ces derniers jours ?", "Avez-vous un mal de gorge apparu ces derniers jours ?", "Avez-vous de la diarrhée ces dernières 24 heures (au moins 3 selles molles) ?", "Avez-vous une fatigue inhabituelle ces derniers jours ?", "Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?", "Avez-vous vu apparaître une gêne respiratoire ou une augmentation de votre gêne respiratoire habituelle ?", "Comment sentez-vous ?", "Avez-vous d’autres symptômes ?", "Quel est votre âge ?", "Quel est votre poids ? Quelle est votre taille ?", "Avez-vous de l’hypertension artérielle ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez vous un traitement à visée cardiologique ?", "Êtes-vous diabétique ?", "Avez-vous ou avez-vous eu un cancer ?", "Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?", "Avez-vous une insuffisance rénale chronique dialysée ?", "Avez-vous une maladie chronique du foie ?", "Êtes-vous enceinte ?", "Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?", "Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive)."];
 
 
 
@@ -20,9 +23,13 @@ window.addEventListener("DOMContentLoaded", ()=>{
 infoPage.style.display='block';
 testPage.style.display='none';
 progressbar.style.display='none';
+bulletTwo.classList.remove('disabled');
+bulletThree.classList.remove('shown');
 
 
 
+
+let currentValue=1;
 //let i=0;
 //let maxQuestions=22;
 testStart.addEventListener('click', ()=>{
@@ -31,7 +38,9 @@ testStart.addEventListener('click', ()=>{
         
         testPage.style.display = 'block';
         progressbar.style.display='block';
-    
+        bulletOne.classList.remove('active');
+        bulletTwo.classList.add('disabled');
+        bulletThree.classList.remove('shown');
         
         
     })
@@ -243,6 +252,15 @@ nextBtn.innerText="Question suivante";
         
         questionProgress.style.display="none";
         progressbar.style.display='none';
+        bulletOne.classList.remove('active');
+        bulletTwo.classList.remove('disabled');
+        bulletThree.classList.add('shown');
+
+        currentValue++;
+        if(currentValue>bullets.length){
+            currentValue=bullets.length;
+        }
+        update();
 
     })
 
@@ -250,10 +268,10 @@ nextBtn.innerText="Question suivante";
  
 }
     
-    else if (i==0)
-    {
+    // else if (i==0)
+    // {
 
-    }
+    // }
 
     answers.classList.add(`.questions__answers'${i}`);
     answers.classList.remove(`.questions__answers'${i-1}`);
@@ -446,6 +464,12 @@ nextBtn.innerText="Question suivante";
         previousBtn.style.display="none";
       const divquest=document.querySelector('.progresspercent__question');
       divquest.style.display="none";
+
+      currentValue--;
+      if(currentValue<1){
+          currentValue=1;
+      }
+      update();
         
     })
     }
@@ -460,12 +484,8 @@ nextBtn.innerText="Question suivante";
 })
 
 
-function changeElement (answer){
-    const input=document.getElementById(answer);
-    input.style.backgroundColor="$textColor9";
-    input.style.border="1px solid $textColor2";
 
-}
+
 
     
 
